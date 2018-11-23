ac2a8c8ffe6a4d87e1a427674e2d270b24b319e1
cristopher1907 <43661233+cristopher1907@users.noreply.github.com>
2018-11-23 09:36:05 -0500

    Add files via upload

---
+++ src User/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule} from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+
+/*let config = new AuthServiceConfig([
+  {
+    id: InstagramLoginProvider.PROVIDER_ID,
+    provider: new InstagramLoginProvider("Instagram-OAuth-Client-Id")
+  },
+]);*/
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'', component: LoginComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+    RouterModule.forRoot(routes), FormsModule
+
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

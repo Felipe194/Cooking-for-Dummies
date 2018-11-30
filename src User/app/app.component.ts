432a503aad866bb01770c78a56b5b923d2bc8559
cristopher1907 <43661233+cristopher1907@users.noreply.github.com>
2018-11-30 08:48:12 -0500

    Update app.component.ts

--- src User/app/app.component.ts
+++ src User/app/app.component.ts
@@ -8,5 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {//implements OnInit
   title = 'app';
 
-
+angular.module('app', [
+  '720kb.socialshare'
+]);
+ 
+app.config(function(socialshareConfProvider){
+   socialshareConfProvider.configure([{
+    'provider': 'twitter',
+    'conf': {
+      'url': 'http://dantecervantes/social-share-con-angular-js',
+      'text': 'Social share con angular.js',
+      'via': 'dcergo',
+      'hashtags': 'angularjs,socialshare,angular-socialshare',
+      'trigger': 'click',
+      'popupHeight': 800,
+      'popupWidth' : 400
+    }
+  },
+  {
+    'provider': 'facebook',
+    'conf': {
+      'url': 'http://dantecervantes/social-share-con-angular-js',
+      'trigger': 'mouseover',
+      'popupHeight': 800,
+      'popupWidth' : 400
+    }
+  }, //aquí puedes añadir las demás opciones por default de cada red social
+  ]);
+})
 }
